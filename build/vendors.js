11dee2155ebfd6b5b02baefb8086791dee5e921a
Mykal Murphree <mmurphree@mk-sea00302-mbp.corp.zulily.com>
2020-02-18 16:54:21 -0800

    First commit

---
+++ build/vendors.js
@@ -0,0 +1,21 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["vendors~main"],{
+
+/***/ "./designDependencies.js":
+/*!*******************************!*\
+  !*** ./designDependencies.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+const designs = {}
+
+// The packages are passed in through a template
+
+
+module.exports = designs
+
+
+/***/ })
+
+}]);
